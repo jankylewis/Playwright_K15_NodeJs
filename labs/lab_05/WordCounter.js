2ef624e74a203fea67df87f79beab8a0802198ac
Janky Lewis <jankylewis.sae.01@gmail.com>
2024-07-28 14:15:50 +0700

    Completed Lab 05

--- labs/lab_05/WordCounter.js
+++ labs/lab_05/WordCounter.js
@@ -1,3 +1,10 @@
+const userString = require('readline-sync').question('Enter your String: ');
 
+let wordsSplitted = userString.split(' ');
+let wordCounts = {}
 
+for (word of wordsSplitted) 
+    wordCounts[word] = (wordCounts[word] || 0) + 1;
+
+console.log(wordCounts);
 
